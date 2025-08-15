fd5c3a6a02c1a09a70dea6d49c54823f4b7c523f
milbrab-rs <84333008+milbrab-rs@users.noreply.github.com>
2025-08-15 07:08:35 -0500

    Update prisma-defender-com-linux.sh

--- prisma-defender-com-linux.sh
+++ prisma-defender-com-linux.sh
@@ -1,13 +1,15 @@
+#!/usr/bin/env bash
+
 # If using SaaS, PCC_USER and PCC_PASS will be an access key and secret key.
-PCC_USER=$(gcloud secrets versions access 1 --secret="pcc_user")
-PCC_PASS=$(gcloud secrets versions access 1 --secret="pcc_pass")
+PCC_USER=""
+PCC_PASS=""
 
 # If using SaaS, PCC_URL should be the exact value copied from
 # Compute > Manage > System > Utilities > Path to Console
-PCC_URL=$(gcloud secrets versions access 1 --secret="pcc_url")
+PCC_URL=""
 
 # This will be PCC_URL without the scheme prefix and any path suffix
-PCC_DOMAIN_NAME=$(gcloud secrets versions access 1 --secret="pcc_domain")
+PCC_DOMAIN_NAME=""
 
 json_auth_data="$(printf '{ "username": "%s", "password": "%s" }' "${PCC_USER}" "${PCC_PASS}")"
 token=$(curl -sSLk -d "$json_auth_data" -H 'content-type: application/json' "$PCC_URL/api/v1/authenticate" | python3 -c 'import sys, json; print(json.load(sys.stdin)["token"])')
