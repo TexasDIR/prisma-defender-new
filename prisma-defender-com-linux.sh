7f112d33aba5a1b2985d7bcc800f63ba09cc483b
milbrab-rs <84333008+milbrab-rs@users.noreply.github.com>
2025-08-18 07:43:02 -0500

    Update prisma-defender-com-linux.sh

--- prisma-defender-com-linux.sh
+++ prisma-defender-com-linux.sh
@@ -11,6 +11,6 @@ PCC_URL=""
 # This will be PCC_URL without the scheme prefix and any path suffix
 PCC_DOMAIN_NAME=""
 
-json_auth_data="$(printf '{ "username": "%s", "password": "%s" }' "${PCC_USER}" "${PCC_PASS}")"
-token=$(curl -sSLk -d "$json_auth_data" -H 'content-type: application/json' "$PCC_URL/api/v1/authenticate" | python3 -c 'import sys, json; print(json.load(sys.stdin)["token"])')
+export json_auth_data="$(printf '{ "username": "%s", "password": "%s" }' "${PCC_USER}" "${PCC_PASS}")"
+export token=$(curl -sSLk -d "$json_auth_data" -H 'content-type: application/json' "$PCC_URL/api/v1/authenticate" | python3 -c 'import sys, json; print(json.load(sys.stdin)["token"])')
 curl -sSLk -H "authorization: Bearer $token" -X POST "$PCC_URL/api/v1/scripts/defender.sh" | bash -s -- -c "$PCC_DOMAIN_NAME" -d "none" --install-host
